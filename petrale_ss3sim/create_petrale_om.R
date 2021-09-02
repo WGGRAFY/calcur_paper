c1581268a7025b389ac66cff17f9efae4cc6a7e4
Ono <kotaro.ono@hi.no>
2021-09-02 10:45:39 +0200

    Corrected LAA extracted  + code to add observation error

--- petrale_ss3sim/create_petrale_om.R
+++ petrale_ss3sim/create_petrale_om.R
@@ -57,7 +57,9 @@ system.file("bin", package = "ss3sim")
   Nage = as.numeric(substr(Datfile[grep("#_Nages", Datfile, fixed=TRUE)], 1, 2))
   # if length bin method is 2 (like in this case) do below:
   Nlength =  (as.numeric(substr(Datfile[grep("# maximum size ", Datfile, fixed=TRUE)], 1, 3)) - as.numeric(substr(Datfile[grep("# minimum size ", Datfile, fixed=TRUE)], 1, 2)))/as.numeric(substr(Datfile[grep("# binwidth for population", Datfile, fixed=TRUE)], 1, 2))+1
-
+  StartYr = as.numeric(substr(Datfile[grep("#_StartYr", Datfile, fixed=TRUE)], 1, 4))
+  EndYr = as.numeric(substr(Datfile[grep("#_EndYr", Datfile, fixed=TRUE)], 1, 4))
+  
 ## now read in the Report.sso file
   rawrep <- read.table(file=paste0(getwd(), "/calcur_paper-master1/petrale_ss3sim/om_test/Report.sso"),col.names=1:200,fill=TRUE,quote="", colClasses="character",nrows=-1,comment.char="")
 
@@ -65,21 +67,23 @@ system.file("bin", package = "ss3sim")
   LAA <- matchfun2(string1 ="MEAN_SIZE_TIMESERIES", adjust1=1, string2="mean_size_Jan_1_for_sex", adjust2=-2, header = TRUE)
 
 # size from subseason 1 and morph 1
-  LAA_sea1_morph1 <- LAA %>% filter(Morph==1, SubSeas==1)
+  LAA_sea1_morph1 <- LAA %>% filter(Morph==1, SubSeas==1, Yr>=StartYr, Yr <= EndYr) 
  
 # size from subseason 1 and morph 2
-  LAA_sea1_morph2 <- LAA %>% filter(Morph==1, SubSeas==2)
+  LAA_sea1_morph2 <- LAA %>% filter(Morph==1, SubSeas==2, Yr>=StartYr, Yr <= EndYr)
   
 # size from subseason 2 and morph 1
-  LAA_sea2_morph1 <- LAA %>% filter(Morph==2, SubSeas==1)
+  LAA_sea2_morph1 <- LAA %>% filter(Morph==2, SubSeas==1, Yr>=StartYr, Yr <= EndYr)
   
 # size from subseason 2 and morph 2
-  LAA_sea2_morph2 <- LAA %>% filter(Morph==2, SubSeas==2)
+  LAA_sea2_morph2 <- LAA %>% filter(Morph==2, SubSeas==2, Yr>=StartYr, Yr <= EndYr)
   
 
 # We then need to add some observation error on top of them to create some data to feed in the estimation model
   
-  
+  CV <- 0.1
+  SD = sqrt(log(CV^2+1)) # for lognromal distribution
+  LAA_dat <- t(apply(LAA_sea1_morph1[,-c(1:4)],1,function(x) as.numeric(as.character(x))))*rlnorm(prod(dim(LAA_sea1_morph1[,-c(1:4)])), 0, SD)
   
   
   
