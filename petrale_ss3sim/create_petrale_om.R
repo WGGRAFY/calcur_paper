abcd379ea4a5b1e73353120f8615a921c6588a46
Christine Stawitz.pc <Christine.Stawitz.pc@nmfs.local>
2021-09-03 11:50:58 -0700

    Use here and dplyr to split

--- petrale_ss3sim/create_petrale_om.R
+++ petrale_ss3sim/create_petrale_om.R
@@ -1,15 +1,16 @@
 #install
+install.packages("here")
 remotes::install_github("r4ss/r4ss", ref="development")
 require(r4ss)
 remotes::install_github("ss3sim/ss3sim",
                         ref = "main", build_vignettes = TRUE, dependencies = TRUE)
 require(ss3sim)
+require(purrr)
+require(here)
 
 
 #look for ss.exe
 system.file("bin", package = "ss3sim")
-
-
 
 
 ### Creating scenarios
@@ -23,37 +24,10 @@ system.file("bin", package = "ss3sim")
 ### What do we need? length at age information
 
 
-## Now some function to extract the LAA info from the SS3 runs 
-## (the 2 functions below are taken from https://github.com/r4ss/testing/blob/master/r4ss%20tables.r)
-  emptytest <- function(x){ sum(!is.na(x) & x=="")/length(x) }
-  
-  matchfun2 <- function(string1,adjust1,string2,adjust2,cols="nonblank",matchcol1=1,matchcol2=1,
-                        objmatch=rawrep,objsubset=rawrep,substr1=TRUE,substr2=TRUE,header=FALSE)
-  {
-    # return a subset of values from the report file (or other file)
-    # subset is defined by character strings at the start and end, with integer
-    # adjustments of the number of lines to above/below the two strings
-    line1 <- match(string1,if(substr1){substring(objmatch[,matchcol1],1,nchar(string1))}else{objmatch[,matchcol1]})
-    line2 <- match(string2,if(substr2){substring(objmatch[,matchcol2],1,nchar(string2))}else{objmatch[,matchcol2]})
-    if(is.na(line1) | is.na(line2)) return("absent")
-    
-    if(is.numeric(cols))    out <- objsubset[(line1+adjust1):(line2+adjust2),cols]
-    if(cols[1]=="all")      out <- objsubset[(line1+adjust1):(line2+adjust2),]
-    if(cols[1]=="nonblank"){
-      # returns only columns that contain at least one non-empty value
-      out <- objsubset[(line1+adjust1):(line2+adjust2),]
-      out <- out[,apply(out,2,emptytest) < 1]
-    }
-    if(header && nrow(out)>0){
-      out[1,out[1,]==""] <- "NoName"
-      names(out) <- out[1,]
-      out <- out[-1,]
-    }
-    return(out)
-  }
 
 ## Now read in the dat file to extract some important info
-  Datfile = readLines(con=paste0(getwd(), "/calcur_paper-master1/petrale_ss3sim/om_test/om.dat", sep=""))
+# Directory needs to be named "om" to work with ss3sim
+  Datfile = readLines(here("om/om.dat"))
   Nage = as.numeric(substr(Datfile[grep("#_Nages", Datfile, fixed=TRUE)], 1, 2))
   # if length bin method is 2 (like in this case) do below:
   Nlength =  (as.numeric(substr(Datfile[grep("# maximum size ", Datfile, fixed=TRUE)], 1, 3)) - as.numeric(substr(Datfile[grep("# minimum size ", Datfile, fixed=TRUE)], 1, 2)))/as.numeric(substr(Datfile[grep("# binwidth for population", Datfile, fixed=TRUE)], 1, 2))+1
@@ -66,6 +40,7 @@ system.file("bin", package = "ss3sim")
 # Extract the general place where the AGE_LENGTH_KEY is written 
   LAA <- matchfun2(string1 ="MEAN_SIZE_TIMESERIES", adjust1=1, string2="mean_size_Jan_1_for_sex", adjust2=-2, header = TRUE)
 
+  LAA %>% purrr::map(c(1,2), filter(Morph==.x))
 # size from subseason 1 and morph 1
   LAA_sea1_morph1 <- LAA %>% filter(Morph==1, SubSeas==1, Yr>=StartYr, Yr <= EndYr) 
  
