d2f976517a2a2e56a14b22b3960ce8710abb3497
Sean Anderson <sean@seananderson.ca>
2022-03-14 17:44:20 -0700

    Estimate and plot all 3 SARLA types?

--- code/sarla_model/calcurr.R
+++ code/sarla_model/calcurr.R
@@ -57,9 +57,6 @@ for (i in seq_len(length(spp$spp))) {
     mutate_all(~ replace(., is.na(.), 999))
 }
 
-
-
-
 petrale_data <- t(model_data[[7]][, -c(1, 2, 22)])
 lingcod_data <- t(model_data[[6]][, -c(1,13,14)])
 
@@ -72,14 +69,48 @@ realdat$Ncohorts <- realdat$Nages + realdat$Nyears - 1
 stan_dat <- plot_and_fill_data(realdat, init_effects = 0, plot=T)
 names(stan_dat)[1] <- "laa_obs"
 
+stan_dat$sigma_o_prior <- c(log(0.5), 0.5)
+stan_dat$sigma_p_prior <- c(log(0.2), 0.2)
+
 # quick testing, adjust path as needed:
 library(cmdstanr)
 file.remove("../sarla/inst/stan/sarla")
 mod <- cmdstan_model("../sarla/inst/stan/sarla.stan")
 
+# year effects:
 stan_dat$N_eta_c <- 0L
-stan_dat$sigma_o_prior <- c(log(0.5), 0.5)
-stan_dat$sigma_p_prior <- c(log(0.2), 0.2)
+
+# cohort effects:
+stan_dat$est_cohort_effects <- 1L
+stan_dat$est_init_effects <- 0L
+stan_dat$est_year_effects <- 0L
+stan_dat$N_delta_c <- stan_dat$Ncohorts
+stan_dat$N_gamma_y <- 0L
+stan_dat$N_eta_c <- 0L
+
+# init effects:
+stan_dat$est_init_effects <- 1L
+stan_dat$est_year_effects <- 0L
+stan_dat$est_cohort_effects <- 0L
+stan_dat$N_delta_c <- 0L
+stan_dat$N_gamma_y <- 0L
+stan_dat$N_eta_c <- stan_dat$Ncohorts
+
+# year and cohort effects??
+stan_dat$est_cohort_effects <- 1L
+stan_dat$est_init_effects <- 0L
+stan_dat$est_year_effects <- 1L
+stan_dat$N_delta_c <- stan_dat$Ncohorts
+stan_dat$N_gamma_y <- stan_dat$Ncohorts
+stan_dat$N_eta_c <- 0L
+
+# all 3!?
+stan_dat$est_cohort_effects <- 1L
+stan_dat$est_init_effects <- 1L
+stan_dat$est_year_effects <- 1L
+stan_dat$N_delta_c <- stan_dat$Ncohorts
+stan_dat$N_gamma_y <- stan_dat$Ncohorts
+stan_dat$N_eta_c <- stan_dat$Ncohorts
 
 fit <- mod$sample(
   data = stan_dat,
@@ -92,10 +123,6 @@ fit <- mod$sample(
 )
 fit
 
-# Look at fitted model
-fit$summary(variables = c("sigma_p", "sigma_o", "beta", "xaa[1,10]", "xaa[2,10]", "gamma_y[1]", "gamma_y[2]", "X0[1]"))
-fit$cmdstan_diagnose()
-
 post <- posterior::as_draws_df(fit$draws())
 pars <- names(post)
 pars <- pars[!grepl("raw", pars)]
@@ -103,6 +130,9 @@ pars_main <- pars[unique(c(
   grep("_sd", pars),
   grep("sigma_", pars), grep("beta", pars), grep("sigma_", pars)
 ))]
+
+fit$summary(variables = c("sigma_p", "sigma_o", "beta", "xaa[1,10]", "xaa[2,10]", "gamma_y[1]", "gamma_y[2]", "X0[1]", "gamma_y_sd"))
+fit$cmdstan_diagnose()
 
 bayesplot::mcmc_areas_ridges(fit$draws(pars_main))
 bayesplot::mcmc_trace(fit$draws(pars_main))
@@ -118,18 +148,18 @@ quantile_summary <- post_xaa %>%
   summarise(
     lwr = quantile(.value, 0.1),
     upr = quantile(.value, 0.9),
-    med = median(.value)
+    med = median(.value), .groups = "drop"
   )
 
-legend_def <- c("median" = "black", "95 quantile" = "gray")
+# legend_def <- c("median" = "black", "95 quantile" = "gray")
 quantile_summary %>%
   filter(!(lwr == 0 & upr == 0)) %>%
   ggplot(aes(age, med, ymin = lwr, ymax = upr)) +
   facet_wrap(~year) +
-  geom_line(alpha = 1, aes(colour = "red")) +
+  geom_line(alpha = 1) +
   geom_ribbon(alpha = 0.5) +
   theme_minimal() +
-  scale_colour_manual(values = legend_def) +
+  # scale_colour_manual(values = legend_def) +
   theme(
     legend.position = c(.90, .05),
     legend.key.size = unit(0.1, "cm"),
@@ -137,25 +167,44 @@ quantile_summary %>%
     legend.text = element_text(size = "6")
   )
 
-# if (stan_dat$est_cohort_effects) {
-#   post_delta_c <- tidybayes::gather_draws(fit, delta_c[y])
-#   delta_hat <- post_delta_c %>%
-#     group_by(y) %>%
-#     summarise(med = median(.value))
-# }
+quantile_summary %>%
+  filter(!(lwr == 0 & upr == 0)) %>%
+  ggplot(aes(year, med, ymin = lwr, ymax = upr)) +
+  facet_wrap(~age) +
+  geom_line(alpha = 1) +
+  geom_ribbon(alpha = 0.5) +
+  theme_minimal() +
+  # scale_colour_manual(values = legend_def) +
+  theme(
+    legend.position = c(.90, .05),
+    legend.key.size = unit(0.1, "cm"),
+    legend.title = element_text(size = "6"),
+    legend.text = element_text(size = "6")
+  )
 
-# if (stan_dat$est_init_effects) {
-#   post_eta_c <- tidybayes::gather_draws(fit, eta_c[y])
-#   eta_hat <- post_eta_c %>%
-#     group_by(y) %>%
-#     summarise(med = median(.value))
-#   eta_lwr <- post_eta_c %>%
-#     group_by(y) %>%
-#     summarise(lwr = quantile(.value, 0.025))
-#   eta_upr <- post_eta_c %>%
-#     group_by(y) %>%
-#     summarise(upr = quantile(.value, 0.975))
-# }
+if (stan_dat$est_cohort_effects) {
+  post_delta_c <- tidybayes::gather_draws(fit, delta_c[y])
+  delta_hat <- post_delta_c %>%
+    group_by(y) %>%
+    summarise(med = median(.value),
+      lwr = quantile(.value, probs = 0.1),
+      upr = quantile(.value, probs = 0.9)
+    )
+  ggplot(delta_hat, aes(y, med, ymin = lwr, ymax = upr)) +
+    geom_pointrange()
+}
+
+if (stan_dat$est_init_effects) {
+  post_eta_c <- tidybayes::gather_draws(fit, eta_c[y])
+  eta_hat <- post_eta_c %>%
+    group_by(y) %>%
+    summarise(med = median(.value),
+      lwr = quantile(.value, probs = 0.1),
+      upr = quantile(.value, probs = 0.9)
+    )
+  ggplot(eta_hat, aes(y, med, ymin = lwr, ymax = upr)) +
+    geom_pointrange()
+}
 
 if (stan_dat$est_year_effects) {
   post_gamma_y <- tidybayes::gather_draws(fit, gamma_y[y])
@@ -180,29 +229,29 @@ if (stan_dat$est_year_effects) {
 #   parallel_chains = parallel::detectCores(),
 #   adapt_delta = 0.95
 # )
-
-summ <- fit$summary()
-require(shinystan)
-stanfit <- rstan::read_stan_csv(fit$output_files())
-launch_shinystan(stanfit)
-
-
-summ[1:81,]
-unique(summ$variable)
-save(fit, file = "code/sarla_model/output/LingcodAnnual.Rds")
-posterior <- fit$draws()
-gamma_draws <- subset_draws(posterior, "gamma_y")
-#Make some plots
-require(bayesplot)
-
-plot_title <- ggtitle("Posterior distributions",
-                      "with medians and 80% intervals")
-mcmc_intervals(gamma_draws,
-           # pars = c(paste("gamma_y[",1:24,"]",sep="")),
-           prob = 0.8) + plot_title
-
-readin <- load("code/sarla_model/output/LingcodAnnual.Rds")
-
-# create a stanfit object - not working
-init_stanfit <- rstan::read_stan_csv(fit$output_files())
-bridge_sampler(samples = init_stanfit)
+#
+# summ <- fit$summary()
+# require(shinystan)
+# stanfit <- rstan::read_stan_csv(fit$output_files())
+# launch_shinystan(stanfit)
+#
+#
+# summ[1:81,]
+# unique(summ$variable)
+# save(fit, file = "code/sarla_model/output/LingcodAnnual.Rds")
+# posterior <- fit$draws()
+# gamma_draws <- subset_draws(posterior, "gamma_y")
+# #Make some plots
+# require(bayesplot)
+#
+# plot_title <- ggtitle("Posterior distributions",
+#                       "with medians and 80% intervals")
+# mcmc_intervals(gamma_draws,
+#            # pars = c(paste("gamma_y[",1:24,"]",sep="")),
+#            prob = 0.8) + plot_title
+#
+# readin <- load("code/sarla_model/output/LingcodAnnual.Rds")
+#
+# # create a stanfit object - not working
+# init_stanfit <- rstan::read_stan_csv(fit$output_files())
+# bridge_sampler(samples = init_stanfit)
